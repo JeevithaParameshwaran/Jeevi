4816c2309c22075191d75d34ae4b16f4d7182514
JeevithaParameshwaran <30828380+JeevithaParameshwaran@users.noreply.github.com>
2018-03-06 16:53:07 +0530

    Create areatriangle.c

---
+++ areatriangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    float b,h,a;
+    printf("Enter base and height");
+    scanf("%f%f",&b,&h);
+    a=0.5*b*h;
+    printf("The area of triangle is:%f",a);
+}
+
